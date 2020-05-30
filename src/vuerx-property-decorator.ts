bc66757c999c60b023f64545031db36c58ac5cd1
Frits Stegmann <fstegmann@gmail.com>
2020-05-30 11:30:27 +0200

    failed attempt at adding subscriptions to components

--- src/vuerx-property-decorator.ts
+++ src/vuerx-property-decorator.ts
@@ -1,11 +1,8 @@
 /// <reference types='reflect-metadata'/>
+import Vue from 'vue';
 import {createDecorator} from 'vue-class-component'
+import {isObservable, Observable, Subscription as RxSub} from "rxjs";
 
-/**
- * decorator of a dom stream
- * @param  options the options for the prop
- * @return PropertyDecorator | void
- */
 export function DomStream() {
     return (target: any, key: string) => {
         createDecorator((componentOptions, key) => {
@@ -17,6 +14,41 @@ export function DomStream() {
                 // @ts-ignore
                 componentOptions.domStreams = [key];
             }
+        })(target, key)
+    }
+}
+
+export function defineReactive (vm: any, key: string, val: any) {
+    if (key in vm) {
+        vm[key] = val;
+    } else {
+        // @ts-ignore
+        Vue.util.defineReactive(vm, key, val);
+    }
+}
+
+export function Subscription(observable: any) {
+
+    return (target: any, key: string) => {
+        createDecorator((componentOptions, k) => {
+            target.$observables = target.$observables || {}
+
+            // @ts-ignore
+            target._subscription = target._subscription || new RxSub()
+            defineReactive(target, key, undefined)
+
+            // @ts-ignore
+            const obs = target.$observables[k] = observable
+            if (!isObservable(obs)) {
+                console.warn(
+                    'Invalid Observable found in subscriptions option with key "' + key + '".',
+                    target
+                )
+                return
+            }
+            target._subscription.add(obs.subscribe((value: any) => {
+                target[key] = value
+            }, (error: any) => { throw error }))
         })(target, key)
     }
 }
